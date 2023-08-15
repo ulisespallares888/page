f7ae5f5953aea5f30500a875635a3e74cbaa5fae
ulisespallares888 <ulisespallares8@gmail.com>
2023-08-15 19:31:07 -0300

    asd

---
+++ chatpage/chat/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Mensaje(models.Model):
+    contenido = models.TextField()
+    usuario = models.CharField(max_length=50, default='Anonimo')
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
---
+++ chatpage/chat/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from .models import Mensaje
+
+def home(request):
+    client_ip = request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
+    print("Client IP:", client_ip)
+    if request.method == 'POST':
+        contenido = request.POST.get('message-input')
+        usuario = request.POST.get('username-input')
+
+        if usuario == '':
+            usuario = "Anónimo"
+
+        if contenido:
+            mensaje = Mensaje(contenido=contenido, usuario=usuario)
+            mensaje.save()
+
+        return redirect('home')
+
+    mensajes = Mensaje.objects.all().order_by('-fecha_creacion')
+
+    if mensajes.count() > 4:
+        mensaje_ultimo = mensajes.first()
+        mensajes.exclude(pk=mensaje_ultimo.pk).delete()
+
+    mensajes = Mensaje.objects.all().order_by('-fecha_creacion')
+    return render(request, 'index.html', {'mensajes': mensajes})
+
+
